a6fe95b04d74730340632a734dd60c0706bb2498
AbdAziz4212 <abdaziz140401@gmail.com>
2022-04-29 16:40:07 +0800

    init branch Development

--- layouts/landingPage.vue
+++ layouts/landingPage.vue
@@ -1,38 +1,5 @@
 <template>
   <v-app id="inspire">
-    <!-- <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <img
-        src="../static/assets/logo.png"
-        alt="Logo Carpo"
-        height="125"
-        contain
-        class="logo"
-      >
-      
-      <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-        >
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
-    </v-app-bar> -->
-
     <v-app-bar
       app
       color="white"
@@ -66,39 +33,84 @@
           {{ link }}
         </v-tab>
       </v-tabs>
-
-      <!-- <v-btn
-        rounded
-        color="grey darken-3"
-        dark
-      >
-        Login
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        outlined
-        rounded
-        color="grey darken-3"
-      >
-        Register
-      </v-btn> -->
       <v-btn
         rounded
         color="darken-1 pa-5"
         dark
       >
         <v-icon class="mr-1">
-          mdi-cloud-upload
+          mdi-cloud-download
         </v-icon>
          Download Apps
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <!-- <v-container> -->
         <Nuxt />
-      <!-- </v-container> -->
     </v-main>
+  
+    <v-footer
+      absolute
+      app
+      padless
+    >
+      <v-card
+        class="flex"
+        flat
+        tile
+      >
+          <v-row no-gutters space-between>
+            <v-col cols="12" sm="6" class="pa-10">
+              <v-avatar
+                class="mr-3"
+                size="60"
+              >
+                <v-img
+                  contain
+                  max-height="60%"
+                  src="/assets/logocarpo.png"
+                ></v-img>
+              </v-avatar>
+              <label class="font-weight-bold ma-0">Car</label><label class="font-weight-regular ma-0">po</label>
+              <div class="subtitle-1 ml-4">First premium rental car in Indonesia</div>
+            </v-col>
+            <v-col cols="12" sm="3" class="pa-10">
+              <div class="text-h6">About Us</div>
+              <div v-for="about in aboutus" :key="about" class="subtitle-1">
+                {{ about }}
+              </div>
+            </v-col>
+            <v-col cols="12" sm="3" class="pa-10">
+              <div class="text-h6">About Us</div>
+              <div v-for="about in aboutus" :key="about" class="subtitle-1">
+                {{ about }}
+              </div>
+            </v-col>
+          </v-row>
+          
+        </v-card-title>
+
+        <v-card-title>
+          <div class="subheading font-weight-medium">
+            <v-icon class="mr-1">
+              mdi-copyright
+            </v-icon>
+            CopyRight {{ new Date().getFullYear() }}
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
@@ -111,6 +123,18 @@
         'Futures',
         'Help',
       ],
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+      aboutus: [
+        'why Carpo',
+        'Contact Us',
+        'Partnership'
+      ],
+      fixed: false,
     }),
   }
 </script>
