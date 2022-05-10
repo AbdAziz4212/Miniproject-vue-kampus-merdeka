264fb6e219434b0e6c7aa800b382f81bea03a589
AbdAziz4212 <abdaziz140401@gmail.com>
2022-05-10 16:17:22 +0800

    Cars (Search, Detail) , Login Page | -Dinamis

---
+++ components/Carouselcar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-carousel
+    cycle
+    height="100vh"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: 'CarouselCar',
+  data () {
+    return {
+      items: [
+        {
+          src: 'https://images.unsplash.com/photo-1618353482480-61ca5a9a7879?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1583918003351-41c1bbe74a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1537984822441-cff330075342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1579044924785-6fdd8ef713b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-row align="center">
+      <v-col cols="12" sm="12" md="6" class="ma-0 pa-16">
+        <Loginform />
+      </v-col>
+      <v-col cols="12" sm="12" md="6" class="ma-0 pa-0">
+        <Carouselcar />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+    }
+  },
+  head: {
+    title: 'Login'
+  },
+}
+</script>
+
+<style scoped>
+.city-title {
+  letter-spacing: 4px;
+}
+.v-main {
+  background-color: white;
+}
+</style>
